7aebcecfff7bb01caeffa61e4835aabc9bcc5bc2
Len <mqbest123@gmail.com>
2025-11-27 21:19:56 +0900

    프로젝트

---
+++ src/main/java/com/len/high_concurrency_ticketing_service/HighConcurrencyTicketingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.len.high_concurrency_ticketing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyTicketingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighConcurrencyTicketingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/len/high_concurrency_ticketing_service/HighConcurrencyTicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.len.high_concurrency_ticketing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrencyTicketingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
