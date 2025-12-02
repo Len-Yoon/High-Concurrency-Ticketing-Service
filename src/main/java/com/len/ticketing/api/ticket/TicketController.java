754c38c34221c470e6e361ae215c7cff452fb61e
Len <mqbest123@gmail.com>
2025-12-02 21:47:12 +0900

    TicketController

---
+++ src/main/java/com/len/ticketing/api/ticket/TicketController.java
@@ -0,0 +1,37 @@
+package com.len.ticketing.api.ticket;
+
+import com.len.ticketing.api.ticket.dto.HoldSeatRequest;
+import com.len.ticketing.api.ticket.dto.HoldSeatResponse;
+import com.len.ticketing.api.ticket.dto.ReleaseSeatRequest;
+import com.len.ticketing.application.ticket.TicketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/tickets")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    // 좌석 선점
+    @PostMapping("/hold")
+    public HoldSeatResponse hold(@RequestBody HoldSeatRequest request) {
+        var result = ticketService.holdSeat(
+                request.scheduleId(),
+                request.seatNo(),
+                request.userId()
+        );
+        return new HoldSeatResponse(result.success(), result.message());
+    }
+
+    // 좌석 선점 해제
+    @PostMapping("/release")
+    public void release(@RequestBody ReleaseSeatRequest request) {
+        ticketService.releaseSeat(
+                request.scheduleId(),
+                request.seatNo(),
+                request.userId()
+        );
+    }
+}
