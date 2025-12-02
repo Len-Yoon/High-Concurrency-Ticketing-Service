f5d76f8d8a214306aa712cdc9e921e743aa1714d
Len <mqbest123@gmail.com>
2025-12-02 21:39:16 +0900

    Seat JPA 리포지토리에 조회 메서드 추가

--- src/main/java/com/len/ticketing/infra/concert/ScheduleJpaRepository.java
+++ src/main/java/com/len/ticketing/infra/concert/ScheduleJpaRepository.java
@@ -1,10 +1,14 @@
 package com.len.ticketing.infra.concert;
 
 import com.len.ticketing.domain.concert.Schedule;
+import com.len.ticketing.domain.concert.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ScheduleJpaRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findByConcertIdOrderByShowAtAsc(Long concertId);
+
+    Optional<Seat> findByScheduleIdAndSeatNo(Long scheduleId, String seatNo);
 }
