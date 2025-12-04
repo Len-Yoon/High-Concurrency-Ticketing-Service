db6f804934424cdc34a585d6eaaf906c39a63510
Len <mqbest123@gmail.com>
2025-12-04 21:39:26 +0900

    조회 메소드 추가

--- src/main/java/com/len/ticketing/infra/reservation/ReservationJpaRepository.java
+++ src/main/java/com/len/ticketing/infra/reservation/ReservationJpaRepository.java
@@ -3,5 +3,10 @@ package com.len.ticketing.infra.reservation;
 import com.len.ticketing.domain.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationJpaRepository extends JpaRepository<Reservation, Long> {
+
+    // 조회 메서드 추가
+    List<Reservation> findByUserId(Long userId);
 }
