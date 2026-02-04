0068481b4142855c3a68d4db48271d1f67827fb5
Len <mabest123@naver.com>
2026-02-04 22:31:23 +0900

    QueueStatusResponse에 token/expiresAt 추가

--- src/main/java/com/len/ticketing/api/queue/dto/QueueStatusResponse.java
+++ src/main/java/com/len/ticketing/api/queue/dto/QueueStatusResponse.java
@@ -1,6 +1,8 @@
 package com.len.ticketing.api.queue.dto;
 
 public record QueueStatusResponse(
-        long position,
-        boolean canEnter
+        long position,        // canEnter면 0
+        boolean canEnter,
+        String token,         // canEnter면 내려줌
+        Long expiresAt        // epoch ms
 ) {}
