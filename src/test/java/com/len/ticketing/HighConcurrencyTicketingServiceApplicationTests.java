16f626a17660e6fa4bb4b6803b332a503d869d56
Len <mabest123@naver.com>
2026-02-11 21:48:02 +0900

    테스트 클래스 최종본

--- src/test/java/com/len/ticketing/HighConcurrencyTicketingServiceApplicationTests.java
+++ src/test/java/com/len/ticketing/HighConcurrencyTicketingServiceApplicationTests.java
@@ -2,12 +2,26 @@ package com.len.ticketing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest
+@ActiveProfiles("test")
 class HighConcurrencyTicketingServiceApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static final MySQLContainer<?> mysql =
+            new MySQLContainer<>("mysql:8.4")
+                    .withDatabaseName("ticketing")
+                    .withUsername("test")
+                    .withPassword("test");
 
     @Test
     void contextLoads() {
     }
-
 }
